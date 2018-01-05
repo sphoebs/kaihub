6af8a76cb451813317f16f816493aa2170d01949
Fabio Casati <fabio.casati@gmail.com>
2018-01-05 16:59:54 +0100

    initial commit

---
+++ clgenerator.js
@@ -0,0 +1,36 @@
+
+
+
+function generateBinaryClassifiers(numClassifiers, betaDistributionParameters, correlation) {
+
+    const classifiers = []
+
+    if (distribution.type = 'normal') {
+
+    }
+
+
+
+    return classifiers
+
+}
+
+function classifyBinary(classifier, item) {
+
+    const classEstimation = {
+        classifier,
+        item,
+        classProbabilities:[],
+        estimatedClass: null, // class corresponding to max probability
+        probability: null,// corresponding probability
+    }
+
+    const accuracy = classifier.accuracyifTrueClassIs(item.trueClass)
+    const randomNumber = Math.random()
+    if (randomNumber<accuracy) estimation =
+
+    return classEstimation
+}
+
+
+
